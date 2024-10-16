d773f89b4c2662b15f342f7b80eb3721a7c3f110
Ritesh Dhote <dhoteritesh66@gmail.com>
2024-10-16 23:42:05 +0530

    add file

---
+++ Day-2/script.js
@@ -0,0 +1,35 @@
+// function even(num){
+//     sum = 0;
+//     for(i=1;i<=num;i++){
+//         if(i%2 == 0){
+//             sum += i
+//         }
+//         // console.log(i)
+//     }
+//     console.log(i)
+// }
+
+// even(5)
+
+
+// let obj = {
+//     name : "ritesh",
+//     lastname: "dhote",
+//     age: 21
+// }
+
+// let sq = obj["name"]
+// console.log(sq)
+
+// let dot = obj.lastname
+// console.log(dot)
+
+let obj = {
+    name : "ritesh",
+    lastname: "dhote",
+    age: 21
+}
+
+obj.name = "rajesh"
+
+console.log(obj)
