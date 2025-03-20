f892c1341677a6865631ca7fb8f8e851af81f373
Ritesh Dhote <dhoteritesh66@gmail.com>
2025-03-20 15:43:28 +0530

    add file

---
+++ Day-12/index.js
@@ -0,0 +1,20 @@
+function binarySearch(arr, target) {
+    
+    let left = 0;
+    
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (arr[mid] === target) 
+            return mid;
+        else if (arr[mid] < target) 
+            left = mid + 1;
+        else 
+        right = mid - 1;
+    }
+    return -1;
+}
+
+let arr = [10, 20, 30, 40, 50];
+console.log(arr[4]); 
